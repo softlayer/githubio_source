f7667b3093f3f546f8a72c77ba7ee13c7ad36ddc
Christopher Gallo <cgallo@us.ibm.com>
2024-02-21 17:35:25 -0600

    working on release note generator

--- bin/releaseNotes.py
+++ bin/releaseNotes.py
@@ -7,10 +7,40 @@ import requests
 import json
 from pprint import pprint as pp
 # from http.client import HTTPConnection
+from rich.console import Console
+
+def printIssues(issues):
+    for issue in issues:
+        fields = issue.get('fields')
+        print(f"{issue.get('key')} {fields.get('summary')}")
+
+
+class JiraAPI():
+    def __init__(self, apitoken):
+        self.apitoken = apitoken
+        self.jira_url = 'https://jira.softlayer.local/rest/api/2'
+        self.headers = {"Authorization": f"Bearer {self.apitoken}", "Content-Type": "application/json"}
+        self.console = Console()
+
+    def getReleaseJiras(self):
+        jql_project = """project = JIRA"""
+        jql_type = """type = "Production Release" """
+        jql_start = """ "Release Estimated Start Time" >= startOfDay(-7d) """
+        jql_orderby = """ORDER BY "Release Estimated Start Time" DESC"""
+        jql_status = """status = Resolved"""
+        jql = f"{jql_project} AND {jql_type} AND {jql_start} AND {jql_status} {jql_orderby}"
+        apirequest = f"{self.jira_url}/search?jql={jql}"
+        result = requests.get(apirequest, headers=self.headers, verify=False)
+        if result.status_code == 200:
+            raw_json = result.json()
+            self.console.print_json(data=raw_json)
+            return raw_json
+            # printIssues(raw_json.get('issues', []))
+        else:
+            print(f"ERROR {result.status_code}: {result.text}")
+        return None
 
 # Color Chart: https://rich.readthedocs.io/en/stable/appendix/colors.html#appendix-colors
-jira_url = 'https://jira.softlayer.local/rest/api/2'
-# https://jira.softlayer.local/issues/?filter=26578&jql=project%20%3D%20JIRA%20AND%20type%20%3D%20"Production%20Release"%20AND%20created%20>%3D%20startOfDay(-90d)%20ORDER%20BY%20"Release%20Estimated%20Start%20Time"%20DESC
 @click.command()
 @click.option('--debug', default=False, is_flag=True, help="Show debug level output")
 def cli(debug):
@@ -19,19 +49,11 @@ def cli(debug):
     apitoken = os.getenv('SL_JIRA_TOKEN')
     if not apitoken:
         raise click.UsageError("APITOKEN env variable is not set")
-    else:
-        print(f"APITOKEN={apitoken}")
 
-    print("Ok Doing the thing")
-    jql = """project%20%3D%20JIRA%20AND%20type%20%3D%20"Production%20Release"%20AND%20created%20>%3D%20startOfDay(-90d)%20ORDER%20BY%20"Release%20Estimated%20Start%20Time"%20DESC"""
-    apirequest = f"{jira_url}/search?jql={jql}"
-    headers = {"Authorization": f"Bearer {apitoken}", "Content-Type": "application/json"}
-    result = requests.get(apirequest, headers=headers, verify=False)
-    if result.status_code == 200:
-        print("OK THIS WORKS")
-        pp(result.json())
-    else:
-        print(f"ERROR: {result.text}")
+    j_api = JiraAPI(apitoken)
+    releases = j_api.getReleaseJiras()
+    printIssues(releases.get('issues'))
+
     
 
 if __name__ == "__main__":
