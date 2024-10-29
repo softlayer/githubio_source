55c4cc5e187318659c2fce009472cdecbaa9b1ec
Christopher Gallo <chrisagallo@gmail.com>
2024-10-29 17:10:16 -0500

    Removed old sldnHTMl stuff

--- sldnHtml/templates/list.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php foreach($this->objects as $item): ?>
-<div class='list-row'>
-    <div class='list-row-title'><a href='/reference/<?= $this->type ?>/<?= $item ?>'> <?=  str_replace('SoftLayer_', '', $item) ?></a></div>
-</div>
-<?php endforeach ?>
